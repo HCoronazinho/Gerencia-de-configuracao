b95b1076e417652bedcbd52338655be5c9a6180f
Henrique <henriquecaron03@gmail.com>
2026-06-23 19:59:01 -0300

    Ajuste testes

--- testes/rotasBackend.test.js
+++ testes/rotasBackend.test.js
@@ -8,24 +8,6 @@ test("GET /lancamentos deve retornar 200", async () => {
   assert.equal(res.status, 200);
 });
 
-test("POST /lancamentos deve criar registro", async () => {
-  const res = await fetch(`${BASE_URL}/lancamentos`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      descricao: "Teste API",
-      data_lancamento: "2026-04-01",
-      valor: 100,
-      tipo_lancamento: "RECEITA",
-      situacao: "PAGO"
-    })
-  });
-
-  assert.equal(res.status, 200);
-});
-
 test("GET /lancamentos retorna lista", async () => {
   const res = await fetch(`${BASE_URL}/lancamentos`);
   const data = await res.json();
